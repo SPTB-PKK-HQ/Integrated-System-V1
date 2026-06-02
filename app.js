faf281ddc0aec3f13c5a8ceeb30c07b0fb16af75
zariffzainudin-netizen <zariff.zainudin@kuskop.gov.my>
2026-06-02 19:06:37 +0800

    Update SCRIPT_URL to new Google Apps Script URL

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let bakulUnsubscribe = null;
 
   // URL APPSCRIPT
-  const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxcsGjonujioNQAwmWd19zBj_tdxL-EfUylNyMYPboeN1trDuDc3nCu90UWis3BjUqF/exec';
+  const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw29HiKFdcHUgAWTpVsyS_PchHOilIUZ9KTXwnQ6i0w6W_jliJ4mMnvuivUgBiW-P1L/exec';
   
   // Google Client ID
   const GOOGLE_CLIENT_ID = '758579492428-rnfev1nkkf2e6qduhujgtfbhudl2j9td.apps.googleusercontent.com';
